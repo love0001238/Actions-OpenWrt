c74145105659c059d75f8ae6a6d4feecd039d92b
love0001238 <39563454+love0001238@users.noreply.github.com>
2020-09-04 10:36:25 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -10,16 +10,17 @@
 rm -rf package/lean/luci-theme-argon
 
 # 下载其它插件
-git clone https://github.com/jerrykuku/lua-maxminddb.git package
-git clone https://github.com/jerrykuku/luci-app-vssr.git package
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package
+mkdir package/ooooo
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/ooooo
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/ooooo
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/ooooo
 
 # 修改默认LAN地址
-sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.8/g' package/base-files/files/bin/config_generate
 
 # 修改固件信息
-sed -i 's/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='Set-saiL '/g' package/lean/default-settings/files/zzz-default-settings
-sed -i 's/DISTRIB_REVISION='R20.8.27'/DISTRIB_REVISION='TEL:15095660155'/g' package/lean/default-settings/files/zzz-default-settings
+sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='Set-saiL '/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/DISTRIB_REVISION='R20.8.27'/DISTRIB_REVISION='TEL:15095660155'/g" package/lean/default-settings/files/zzz-default-settings
 
 # 修改SSH登录信息
 sed -i 's/W I R E L E S S   F R E E D O M/ SET-SAIL     TEL:15095660155/g' package/base-files/files/etc/banner
