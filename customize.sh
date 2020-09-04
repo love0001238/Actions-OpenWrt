c6ff204700ef1b70d1316abe15759084b1ef5144
love0001238 <39563454+love0001238@users.noreply.github.com>
2020-09-04 09:40:25 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,7 +5,10 @@
 #
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update config)
-#
 
+# git clone package
+git clone https://github.com/jerrykuku/lua-maxminddb.git package
+git clone https://github.com/jerrykuku/luci-app-vssr.git package
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
