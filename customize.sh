de4be151e9abf26b2d067e366363f66fc4345b8c
love0001238 <39563454+love0001238@users.noreply.github.com>
2020-09-04 09:45:37 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,9 +6,16 @@
 # File name: customize.sh
 # Description: OpenWrt DIY script part 2 (After Update config)
 
-# git clone package
+# 下载其它插件
 git clone https://github.com/jerrykuku/lua-maxminddb.git package
 git clone https://github.com/jerrykuku/luci-app-vssr.git package
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package
-# Modify default IP
+
+# 修改默认LAN地址
 sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 修改固件信息
+
+# 修改SSH登录信息
+
+# 删除默认密码
