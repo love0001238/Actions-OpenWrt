87c2cfcc56ea9e546018e16f0482f85835c5403d
love0001238 <39563454+love0001238@users.noreply.github.com>
2020-09-04 09:51:26 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -15,6 +15,8 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package
 sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # 修改固件信息
+sed -i 's/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='Set-saiL '/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/DISTRIB_REVISION='R20.8.27'/DISTRIB_REVISION='TEL:15095660155'/g' package/lean/default-settings/files/zzz-default-settings
 
 # 修改SSH登录信息
 
