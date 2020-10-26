771b0e06b08f2b3a919c16db57bedf61623d88cc
love0001238 <39563454+love0001238@users.noreply.github.com>
2020-10-26 09:19:49 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -14,8 +14,8 @@
 #-------------------------------------------#         #--------------------------------#
 
 # K2硬改配置
-#sed -i 's/<0x50000 0x7b0000>/<0x50000 0x1fb0000>/g' target/linux/ramips/dts/mt7620a_phicomm_psg1218.dtsi
-#sed -i '/phicomm_psg1218a/{n;n;s/7872k/32448k/;}' target/linux/ramips/image/mt7620.mk
+sed -i 's/<0x50000 0x7b0000>/<0x50000 0x1fb0000>/g' target/linux/ramips/dts/mt7620a_phicomm_psg1218.dtsi
+sed -i '/phicomm_psg1218a/{n;n;s/7872k/32448k/;}' target/linux/ramips/image/mt7620.mk
 
 # 删除默认argon主题
 rm -rf package/lean/luci-theme-argon
