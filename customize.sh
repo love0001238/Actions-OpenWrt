c1c49a5f65f5b091870c6989b1e39d68c6efdef8
love0001238 <39563454+love0001238@users.noreply.github.com>
2020-12-05 15:45:22 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -16,10 +16,10 @@
 # 硬改配置
 #sed -i 's/<0x50000 0x7b0000>/<0x50000 0x1fb0000>/g' target/linux/ramips/dts/mt7620a_phicomm_psg1218.dtsi
 #sed -i '/phicomm_psg1218a/{n;n;s/7872k/32448k/;}' target/linux/ramips/image/mt7620.mk
-sed -i '/tl-wr841-v9/{n;s/tplink-4mlzma/tplink-16mlzma/;}' target/linux/ar71xx/image/tiny-tp-link.mk
+#sed -i '/tl-wr841-v9/{n;s/tplink-4mlzma/tplink-16mlzma/;}' target/linux/ar71xx/image/tiny-tp-link.mk
 
 # 删除默认argon主题
-#rm -rf package/lean/luci-theme-argon
+rm -rf package/lean/luci-theme-argon
 
 # 下载其它插件
 #git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
@@ -34,13 +34,13 @@ sed -i 's/192.168.1.1/192.168.3.8/g' package/base-files/files/bin/config_generat
 sed -i 's/ssid=OpenWrt/ssid=OOO/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 修改固件信息
-sed -i "s/DISTRIB_DESCRIPTION='%D %V %C'/DISTRIB_DESCRIPTION='Set-saiL '/g" package/base-files/files/etc/openwrt_release
-sed -i "s/DISTRIB_REVISION='%R'/DISTRIB_REVISION='TEL:15095660155'/g" package/base-files/files/etc/openwrt_release
-#sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='Set-saiL '/g" package/lean/default-settings/files/zzz-default-settings
-#sed -i "s/DISTRIB_REVISION='R20.10.20'/DISTRIB_REVISION='TEL:15095660155'/g" package/lean/default-settings/files/zzz-default-settings
+#sed -i "s/DISTRIB_DESCRIPTION='%D %V %C'/DISTRIB_DESCRIPTION='Set-saiL '/g" package/base-files/files/etc/openwrt_release
+#sed -i "s/DISTRIB_REVISION='%R'/DISTRIB_REVISION='TEL:15095660155'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='Set-saiL '/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/DISTRIB_REVISION='R20.10.20'/DISTRIB_REVISION='TEL:15095660155'/g" package/lean/default-settings/files/zzz-default-settings
 
 # 修改SSH登录信息
 sed -i 's/W I R E L E S S   F R E E D O M/ SET-SAIL     TEL:15095660155/g' package/base-files/files/etc/banner
 
 # 删除默认密码
-#sed -i '/root::0:0/d' package/lean/default-settings/files/zzz-default-settings
+sed -i '/root::0:0/d' package/lean/default-settings/files/zzz-default-settings
