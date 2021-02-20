a8af5b341da72695e1bdd09337362b526bea005d
love0001238 <39563454+love0001238@users.noreply.github.com>
2021-02-20 17:28:23 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -25,6 +25,7 @@ rm -rf package/lean/luci-theme-argon
 #git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 #git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/luci-theme-rosy
 
 # 修改默认名称
 sed -i "s/hostname='OpenWrt'/hostname='Set-saiL'/g" package/base-files/files/bin/config_generate
